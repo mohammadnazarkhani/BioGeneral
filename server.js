4baf6534946c7863db3e3d58f405bc70567b7782
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2024-09-20 22:44:43 +0330

    ejs configuration

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import path from 'path';
 import { fileURLToPath } from 'rul';
 // Routes
 
-
 const PORT = process.env.PORT;
 
 // Get the file and directory names
@@ -11,6 +10,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+// Configure ejs
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 // Body parser middleware
 app.use(express.json());
