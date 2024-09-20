c0078903425c3ad61b669ed2293dda7a3fc30487
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2024-09-20 22:41:40 +0330

    Refactoring: make compatible with es modules rules

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'rul';
+// Routes
+
+
 const PORT = process.env.PORT;
 
+// Get the file and directory names
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Body parser middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Setup static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Import routes
-const navRoutes = require('./routes/navRoutes');
 
-// Use routes
-app.use('/', navRoutes);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Ops! Something broke!');
+});
+
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
-});
-
-app.use((err, req, res, newxt) => {
-  console.error(err.stack);
-  res.status(500).send('Ops! Something broke!');
 });
