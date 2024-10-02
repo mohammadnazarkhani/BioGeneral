ff8967df30eebd1aee12a088df9b7364327245fc
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2024-10-02 17:44:46 +0330

    login controller

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,11 +1,15 @@
 // @desc    Login page
 // @route   GET /login
 export const getLogin = (req, res) => {
-    res.status(200).render('login');
+  res.status(200).render("login");
 };
 
 // @desc    Signin page
 // @route   GET /singin
 export const getSignin = (req, res) => {
-    res.status(200).render('singin');
+  res.status(200).render("singin");
 };
+
+// @desc    Singnin
+// @route   Get /singin
+export const postSingin = (req, res) => {};
